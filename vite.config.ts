65f734587836c243f5745bd5bc3f45c8aa7922ad
bogdanilchenko1987 <bogdan_ilchenko@ukr.net>
2025-05-26 22:52:19 +0300

    udp

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/aussieautowash/",
   plugins: [react()],
-  build: {
-    outDir: "build",
-  },
 });
